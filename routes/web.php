89c41ac52b162054c89d45ffe72fdc1f01c090a5
Vingki <youremail@example.com>
2026-05-09 21:26:51 +0700

    feat: add dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::controller(AuthController::class)->group(function(){
+Route::controller(AuthController::class)->group(function () {
     Route::get('/', 'login')->name('login');
 });
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
